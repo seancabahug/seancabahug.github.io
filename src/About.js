245062221caa1b6fd3dd3a00149c6d81501399e1
Sean Cabahug <25161923+seancabahug@users.noreply.github.com>
2021-11-24 23:00:50 -0700

    minor touches, begin about me

---
+++ src/About.js
@@ -0,0 +1,9 @@
+import './App.css'
+
+export default function About() {
+    return (
+        <div className="App">
+            <h1>WIP, come back later</h1>
+        </div>
+    )
+}
---
+++ src/Home.js
@@ -0,0 +1,36 @@
+import Typist from 'react-typist';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faAward, faUser } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import './App.css';
+
+import 'react-typist/dist/Typist.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <div className="inner">
+        <h1>
+          <Typist cursor={{ element: "_" }}>
+            Sean Cabahug
+          </Typist>
+        </h1>
+        <p>full-stack webdev // cybersecurity // sysadmin</p>
+        <div className="icons">
+          <a href="https://github.com/seancabahug">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <Link to="/about">
+            <FontAwesomeIcon icon={faUser} />
+          </Link>
+          <a href="https://github.com/seancabahug">
+            <FontAwesomeIcon icon={faAward} />
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
