5fe2e0a3c38f61551f3152973d274256063fa726
Sean Cabahug <25161923+seancabahug@users.noreply.github.com>
2022-05-12 11:03:32 -0700

    set up page transitions

--- src/highschool/HighSchool.js
+++ src/highschool/HighSchool.js
@@ -1,13 +1,37 @@
 import {
-    Outlet
+    Outlet,
+    useLocation,
+    Routes,
+    Route,
+    Navigate,
+    Link
 } from 'react-router-dom';
+import { PageTransition } from '@steveeeie/react-page-transition';
 import "../Home.css";
 import "./HighSchool.css";
 
-export default function HighSchool() {
+export function HighSchool() {
+    let location = useLocation();
 
     return <>
-        <h1>yooo</h1>
-        <Outlet />
+        <div>
+            <Link to="resumes">resumes</Link>
+            <Link to="freshman">freshman</Link>
+        </div>
+        <PageTransition 
+            preset="slide" 
+            transitionKey={location.pathname}
+            enterAnimation="moveToLeft"
+            exitAnimation="moveToLeft"
+        >
+            <Routes>
+                <Route index element={<Navigate replace to="resumes" />} />
+                <Route key="resumes" path="resumes" element={<h1>ayyyy</h1>} />
+                <Route key="freshman" path="freshman" element={<h1>f</h1>} />
+                <Route key="sophomore" path="sophomore" element={<h1>s</h1>} />
+                <Route key="junior" path="junior" element={<h1>asdf</h1>} />
+                <Route key="senior" path="senior" element={<h1>ajflajf</h1>} />
+            </Routes>
+        </PageTransition>
     </>;
 }
